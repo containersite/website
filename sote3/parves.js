f486564f97a26b7992913b5cd4f6ee1d02458f10
containersite <sikdarparves527@gmail.com>
2025-11-20 17:20:46 +0600

    Update parves.js

--- sote3/parves.js
+++ sote3/parves.js
@@ -1,2 +1,2 @@
-const newTabURL = "https://watchvarse1.blogspot.com/p/blog-page.html";
-const redirectURL = "https://adx3uq.blogspot.com/p/18-parves.html";
+const newTabURL = "https://www.effectivegatecpm.com/zcvm0rch?key=93b158ea491b4f11e0adbacd15934c67";
+const redirectURL = "https://valianttossczar.com/fz3ifw9n?key=255cb9b54aebe1dca0f89408b47e3fcf";
