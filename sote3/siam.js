026a2dd7971395308ef4d62f83f800a4a4c7d7bd
containersite <sikdarparves527@gmail.com>
2025-11-20 16:18:03 +0600

    Update siam.js

--- sote3/siam.js
+++ sote3/siam.js
@@ -1,1 +1,2 @@
-const redirectURL = "https://www.effectivegatecpm.com/pf098htu?key=9f03649cc82181698ef1df4cf6ec0b38";
+  const newTabURL = "https://www.effectivegatecpm.com/zcvm0rch?key=93b158ea491b4f11e0adbacd15934c67";
+  const redirectURL = "https://valianttossczar.com/fz3ifw9n?key=255cb9b54aebe1dca0f89408b47e3fcf";
