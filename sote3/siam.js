ca118c49cf61610a80d59a9c8fd8015ee11808bb
containersite <sikdarparves527@gmail.com>
2025-11-26 14:04:13 +0600

    Update siam.js

--- sote3/siam.js
+++ sote3/siam.js
@@ -1,2 +1,2 @@
-const newTabURL = "https://valianttossczar.com/j3ut2pg5?key=5b2a76eff91ec7b1aa88b85202a5def6";
-const redirectURL = "https://valianttossczar.com/j3ut2pg5?key=5b2a76eff91ec7b1aa88b85202a5def6";
+const newTabURL = "https://adx3uq.blogspot.com/p/sia.html";
+const redirectURL = "https://adx3uq.blogspot.com/p/sia.html";
