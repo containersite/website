823dae3020956bc841ee2b850c5719f524caca60
containersite <sikdarparves527@gmail.com>
2025-12-11 16:41:06 +0600

    Update parves.js

--- sote3/parves.js
+++ sote3/parves.js
@@ -1,2 +1,2 @@
-const newTabURL = "https://www.zhamster.lol/2025/12/nadia-viral-video-link.html";
+const newTabURL = "https://cutl.io/link12-11-12-25";
 const redirectURL = "https://www.effectivegatecpm.com/zcvm0rch?key=93b158ea491b4f11e0adbacd15934c67";
